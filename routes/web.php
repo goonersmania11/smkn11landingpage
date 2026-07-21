12a07b34a5ca1cf4c866b380685e42052019c54f
ye-00 <mnazriel177@gmail.com.com>
2026-07-21 15:21:06 +0700

    fix directory views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\JurusanController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('jurusan', JurusanController::class);
