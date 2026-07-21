bf8e90734f7db2bafc430eaf4d4a48bc1c171b1a
ye-00 <mnazriel177@gmail.com.com>
2026-07-21 13:39:22 +0700

    feat: create migration, model, and controller for jurusan, guru, and ekstrakurikuler

---
+++ app/Models/guru.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Guru extends Model
+{
+    protected $table = 'guru';
+
+    protected $fillable = [
+        'nama',
+        'nip',
+        'jabatan',
+        'foto'
+    ];
+}
